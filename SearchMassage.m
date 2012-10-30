feb07e6ede68aec54e960dc940a38db39bcc5526
Mars182838 <wj182838@126.com>
2012-10-30 17:15:25 +0800

    become

--- SearchMassage.m
+++ SearchMassage.m
@@ -8,7 +8,7 @@
 @synthesize placeLon = _placeLon;//当前位置的经度
 @synthesize placeMess = _placeMess;//当前位置详细信息
 @synthesize nearbyArray = _nearbyArray;//存放附近信息的数组
-@synthesize searchArray = _searchArray;//
+@synthesize searchArray = _searchArray;//存放搜索的信息数组
 
 -(void)dealloc
 {
